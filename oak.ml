5a59ed17364f065894744020e7a5b8ee991c6fec
Ryan Beckett <rbeckett@princeton.edu>
2015-04-21 17:23:34 -0400

    first implementation attempt

--- oak.ml
+++ oak.ml
@@ -1,5 +1,6 @@
 
 open Map
+open Stack
 
 
 type relation = string
@@ -17,7 +18,7 @@ end
 
 module FM = Map.Make(OrderedField)
 
-type packet = range FM.t
+type packet = (range FM.t) ref
 
 type forwarding_decision =
 	| Deliver
@@ -26,29 +27,33 @@ type forwarding_decision =
 	| Multicast of forwarding_decision * forwarding_decision
 
 type decision_tree =
-	| Root of (decision_tree ref) option
-    | Leaf of forwarding_decision
+	| Dummy
+    | Leaf of packet * forwarding_decision
     | Add of field list * relation * decision_tree ref 
     | Remove of field list * relation * decision_tree ref 
-    | Inrange of range * field * ((packet * (decision_tree ref)) option) * ((packet * (decision_tree ref)) option)
-
+    | Inrange of range * field * (decision_tree ref) * (decision_tree ref)
 
 
 (* Global mutable decision tree *)
-let dtree = ref (Root None)
-let loc = dtree
-
+let root = ref Dummy
+let loc = ref root
+let next_pkts = ref (Stack.create ())
 
 let run (f: packet -> forwarding_decision) : unit = 
-	let sym_pkt = FM.empty in 
-	(* let fd = f sym_pkt; *)
-	()
-
-
+	let sym_pkt = ref FM.empty in 
+	Stack.push sym_pkt !next_pkts;
+	while Stack.is_empty !next_pkts do 
+		let cur_pkt = Stack.pop !next_pkts in 
+		let fd = f cur_pkt in 
+		(!loc) := Leaf (cur_pkt, fd);
+		loc := root
+	done
 
 
 let max_value = 1000
 let min_value = 0
+let total_range = [(min_value,max_value)]
+
 
 let intersection'' ((lo,hi):int*int) (lo',hi') : range = 
 	assert (lo <= hi);
@@ -83,21 +88,72 @@ let complement (r: range) : range =
 
 
 let in_range (p: packet) (f: field) (r: range) : bool = 
-	try 
-	  let r' = FM.find f p in
-	  match intersection r r' with
-	  | [] -> false
-	  | _ -> true
-	with Not_found -> 
-		(match !loc with 
-		 | Leaf fd -> failwith "Not Possible"
-		 | Branches tru fal -> 
-		 	(match tru with 
-		 	 | None -> 
-		 	 	let p' = FM.insert f r p in 
-		 	 	loc := Branches (Some (p', None)) fal
-		 	 | Some (pkt, tree) ->          ) )
+	let aux x y  = 
+		match !(!loc) with 
+		| Dummy -> 
+			let ptru = FM.add f x !p in 
+			let pfal = FM.add f y !p in 
+		 	let new_tru, new_fal = ref Dummy, ref Dummy in 
+		 	(!loc) := Inrange (r,f, new_tru, new_fal);
+		 	(match x,y with 
+		 	 | [], [] -> failwith "Both empty"
+		 	 | [], _ -> 
+		 		loc := new_fal;
+		 		p := pfal;
+		 		false
+		 	 | _, [] -> 
+			 	loc := new_tru;
+			 	p := ptru;
+			 	true
+			 | _, _ -> 
+			 	loc := new_tru;
+			 	p := ptru;
+				Stack.push (ref pfal) !next_pkts;
+			 	true)
+		| Inrange (r',f', tru, fal) -> 
+		 	if r' = r && f' = f 
+		 	then (loc := fal; false)
+		 	else failwith "Error [in_range: different values]"
+		| _ -> failwith "Error [in_range: unhandled case]"
+	in 
+	let r' = try FM.find f !p with _ -> total_range in
+	aux (intersection r r') (intersection (complement r) r') 
+		
 
+(******************************************************
+ *
+ *  Debugging
+ *
+ ******************************************************)
+
+let string_of_field f = 
+	match f with 
+	| IpSrc -> "ipsrc"
+	| IpDst -> "ipdst"
+
+let string_of_interval (lo,hi) = 
+	"lo:" ^ (string_of_int lo) ^ " hi: " ^ (string_of_int hi)
+
+let string_of_range (r : range) = 
+	List.fold_left (fun acc x -> acc ^ (string_of_interval x)) "" r 
+
+let rec repeat_str (n:int) (s:string) = 
+	if n = 0 then ""
+	else s ^ (repeat_str (n-1) s)
+
+let print_dtree (dt: decision_tree) : unit = 
+	let rec aux dt level = 
+		let str = repeat_str level "\t" in 
+		match dt with
+		| Dummy -> print_endline (str ^ "Dummy")
+		| Leaf (pkt,fd) -> print_endline (str ^ "Leaf")
+		| Inrange (r,f,tru,fal) -> 
+			print_endline (str ^ "Inrange: field=" ^ (string_of_field f) ^ " range=" ^ (string_of_range r));
+			aux !fal (level + 1);
+			aux !tru (level + 1)
+		| _ -> print_endline "damn"
+	in 
+	aux dt 0
 
 
 
@@ -107,28 +163,27 @@ let in_range (p: packet) (f: field) (r: range) : bool =
  *
  ******************************************************)
 
-let print_interval (lo,hi) = 
-	print_endline ("lo:" ^ (string_of_int lo) ^ " hi: " ^ (string_of_int hi))
-
-let print_range r = 
-	List.iter print_interval r
-
 let test_intersection () = 
 	let r1 = [(15,55)] in 
 	let r2 = [(10,20); (50,60)] in 
-	print_range (intersection r1 r2)
+	print_endline (string_of_range (intersection r1 r2))
 
 let test_complement1 () = 
 	let r = [(10,20)] in
-	print_range (complement r)
+	print_endline (string_of_range (complement r))
 
 let test_complement2 () = 
 	let r = [] in
-	print_range (complement r)
+	print_endline (string_of_range (complement r))
 
 let test_complement3 () = 
 	let r = [(10,20); (900,max_value)] in
-	print_range (complement r)
+	print_endline (string_of_range (complement r))
+
+let f_simple pkt = 
+	if in_range pkt IpSrc [(10,20)]
+	then Deliver
+	else Drop
 
 let rec run_tests tests = 
 	match tests with 
@@ -139,12 +194,15 @@ let rec run_tests tests =
 		run_tests ts
 
 let () = 
-	let tests = 
+	(* let tests = 
 		[test_intersection; 
 		 test_complement1; 
 		 test_complement2; 
 		 test_complement3] in 
-	run_tests tests
+	run_tests tests; *)
+	run f_simple;
+	print_dtree !root
+
 
 
 
