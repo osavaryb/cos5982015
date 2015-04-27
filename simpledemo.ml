2dad8a5a2261dcfa5f6fc3d69a993ed1cd2f0d50
osavaryb <osavaryb@gmail.com>
2015-04-27 14:37:27 -0400

    ..

--- simpledemo.ml
+++ simpledemo.ml
@@ -20,75 +20,7 @@ let _ = main ()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	  
-
-(*
-	    
-let trusted_ips = [(0,100)]
-
-let firewall pkt = 
-	if in_range pkt IpSrc trusted_ips then 
-		(add pkt [IpDst; IpSrc] "conn";
-		Deliver)
-	else 
-		(if in_relation pkt [IpSrc; IpDst] "conn"
-		then Deliver 
-		else Drop)
-
-*)
-
-
-
-
-
-
 
 
 
